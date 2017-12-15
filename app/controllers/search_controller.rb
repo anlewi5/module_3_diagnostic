a7122628a9b0239e8aca91cb00f32bd8d2ce0cad
Anna Lewis <anna.noel.lewis@gmail.com>
2017-12-15 08:48:40 -0700

    add to search_controller index: returning that API key is invalid

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,15 @@
 class SearchController < ApplicationController
   def index
-    if params[:zip_code]
-      render text: 'hello'
+    zip = params[:zip]
+
+    @conn = Faraday.new(url: "https://api.data.gov") do |faraday|
+      faraday.headers["X-Api-Key"] = "ENV[nrel_key]"
+      faraday.adapter Faraday.default_adapter
     end
-  end
+
+    response = @conn.get("/nrel/alt-fuel-stations/v1.json?zip=#{zip}")
+    binding.pry
+    @stations = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
+    binding.pry
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/search', to: "search#index"
-
-  get '/api/alt-fuel-stations/v1.json?zip'
 end

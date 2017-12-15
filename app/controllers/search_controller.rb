52ccecd41eca4f59dc9d5f7aedc6b850590e3184
Anna Lewis <anna.noel.lewis@gmail.com>
2017-12-15 09:12:24 -0700

    Add station class to hold attributes.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,13 @@
 class SearchController < ApplicationController
   def index
-    zip = params[:zip]
+    location = params[:location]
 
     @conn = Faraday.new(url: "https://api.data.gov") do |faraday|
       faraday.headers["X-Api-Key"] = ENV["nrel_key"]
       faraday.adapter Faraday.default_adapter
     end
 
-    response = @conn.get("/nrel/alt-fuel-stations/v1.json?zip=#{zip}")
+    response = @conn.get("/nrel/alt-fuel-stations/v1/nearest.json?location=#{location}")
     @stations = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
     binding.pry
     end
